074cfac7b9bb368118f42b538c6755db525189a1
ArslanJajja1 <arslanjajja13@gmail.com>
2022-10-19 19:17:05 +0500

    basic recusion problems

---
+++ Recursion/Basic recursion problems.py
@@ -0,0 +1,30 @@
+# # *!Print name 5 times
+# count = 0
+
+
+# def printName(name):
+#     global count
+#     if count == 5:
+#         return
+#     print(name)
+#     count += 1
+#     printName(name)
+
+
+# printName('Arslan')
+# *! Print linearly from 1 to N
+# count = 0
+# def print1ToN(n):
+#     global count
+#     if n == 0:
+#         return
+#     print1ToN(n-1)
+#     print(n)
+# print1ToN(10)
+# *! Print N to 1
+# def printNto1(n):
+#     print(n)
+#     if n == 1:
+#         return
+#     printNto1(n-1)
+# printNto1(10)
