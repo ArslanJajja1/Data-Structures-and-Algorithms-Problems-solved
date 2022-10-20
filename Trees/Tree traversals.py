5ad875902ee079b03e7488434f8b0b8c61a7e710
ArslanJajja1 <arslanjajja13@gmail.com>
2022-10-20 19:32:53 +0500

    inorder traversal

---
+++ Trees/Tree traversals.py
@@ -0,0 +1,50 @@
+import re
+
+
+class Node:
+    def __init__(self, data) -> None:
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+        else:
+            self.data = data
+
+    def printTheTree(self):
+        if self.left:
+            self.left.printTheTree()
+        print(self.data)
+        if self.right:
+            self.right.printTheTree()
+# * Inorder traversal
+# * Left -> Root -> Right
+
+    def inorder(self, root):
+        result = []
+        if root:
+            result = self.inorder(root.left)
+            result.append(root.data)
+            result = result + self.inorder(root.right)
+        return result
+
+
+root = Node(27)
+root.insert(14)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+root.printTheTree()
+print(root.inorder(root))
