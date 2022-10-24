2b69ca6933dc8a30444e94509d089c058a6c482c
ArslanJajja1 <arslanjajja13@gmail.com>
2022-10-24 11:05:01 +0500

    check string palindrome using functional recursion

--- Recursion/Functional recursion problems.py
+++ Recursion/Functional recursion problems.py
@@ -1,3 +1,4 @@
+# #* Reverse an Array :
 def reverseArray(i, arr, n):
     if i >= n/2:
         return
@@ -10,3 +11,19 @@ def reverseArray(i, arr, n):
 arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 reverseArray(0, arr, len(arr))
 print('Reversed Array : ', arr)
+
+# #* Check if a string is palindrome or not
+
+
+def isPalindrome(i, s):
+    n = len(s)
+    if i >= n/2:
+        return True
+    if s[i] != s[n-i-1]:
+        return False
+    return isPalindrome(i+1, s)
+
+
+print('Enter a string to check if palindrome or not !')
+s = input()
+print(isPalindrome(0, s))
