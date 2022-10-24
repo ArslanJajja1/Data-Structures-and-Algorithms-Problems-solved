90e3d79ab85be2a29a1cd1c12be84db254d7572d
ArslanJajja1 <arslanjajja13@gmail.com>
2022-10-24 18:11:41 +0500

    print one subsequence

--- Recursion/subsequences patterns.py
+++ Recursion/subsequences patterns.py
@@ -1,15 +1,36 @@
 # #* Print subsequences whose sum is k
+# def printS(i, arr, n, ds, k, sum):
+#     if i >= n:
+#         if sum == k:
+#             print(ds)
+#         return
+#     ds.append(arr[i])
+#     sum = sum + arr[i]
+#     printS(i+1, arr, n, ds, k, sum)
+#     ds.remove(arr[i])
+#     sum = sum - arr[i]
+#     printS(i+1, arr, n, ds, k, sum)
+
+
+# arr = [1, 2, 1]
+# printS(0, arr, len(arr), [], 2, 0)
+
+# #* Print only 1 subsequence whose sum is k
 def printS(i, arr, n, ds, k, sum):
     if i >= n:
         if sum == k:
             print(ds)
-        return
+            return True
+        return False
     ds.append(arr[i])
     sum = sum + arr[i]
-    printS(i+1, arr, n, ds, k, sum)
+    if printS(i+1, arr, n, ds, k, sum) == True:
+        return True
     ds.remove(arr[i])
     sum = sum - arr[i]
-    printS(i+1, arr, n, ds, k, sum)
+    if printS(i+1, arr, n, ds, k, sum) == True:
+        return True
+    return False
 
 
 arr = [1, 2, 1]
