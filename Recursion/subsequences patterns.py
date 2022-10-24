a4614d510ce7ec1c755cc72988058eb0ab5b8eee
ArslanJajja1 <arslanjajja13@gmail.com>
2022-10-24 18:32:24 +0500

    count all subsequences that satisfy condition

--- Recursion/subsequences patterns.py
+++ Recursion/subsequences patterns.py
@@ -16,22 +16,37 @@
 # printS(0, arr, len(arr), [], 2, 0)
 
 # #* Print only 1 subsequence whose sum is k
-def printS(i, arr, n, ds, k, sum):
+# def printS(i, arr, n, ds, k, sum):
+#     if i >= n:
+#         if sum == k:
+#             print(ds)
+#             return True
+#         return False
+#     ds.append(arr[i])
+#     sum = sum + arr[i]
+#     if printS(i+1, arr, n, ds, k, sum) == True:
+#         return True
+#     ds.remove(arr[i])
+#     sum = sum - arr[i]
+#     if printS(i+1, arr, n, ds, k, sum) == True:
+#         return True
+#     return False
+
+
+# arr = [1, 2, 1]
+# printS(0, arr, len(arr), [], 2, 0)
+# #* Print count of all subsequences that satisfy condition
+def printS(i, arr, n, k, sum):
     if i >= n:
         if sum == k:
-            print(ds)
-            return True
-        return False
-    ds.append(arr[i])
+            return 1
+        return 0
     sum = sum + arr[i]
-    if printS(i+1, arr, n, ds, k, sum) == True:
-        return True
-    ds.remove(arr[i])
+    left = printS(i+1, arr, n, k, sum)
     sum = sum - arr[i]
-    if printS(i+1, arr, n, ds, k, sum) == True:
-        return True
-    return False
+    right = printS(i+1, arr, n, k, sum)
+    return left+right
 
 
 arr = [1, 2, 1]
-printS(0, arr, len(arr), [], 2, 0)
+print(printS(0, arr, len(arr), 2, 0))
