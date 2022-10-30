3ef0e8d83c208ece807a2b60bc30a8c8e13c30a2
ArslanJajja1 <arslanjajja13@gmail.com>
2022-10-30 09:10:33 +0500

    combination sum 2

--- Recursion/Combinations sum.py
+++ Recursion/Combinations sum.py
@@ -1,17 +1,39 @@
 # #* Combination Sum 1
-def findCombinations(index, arr, ans, ds, target):
-    if index == len(arr):
-        if target == 0:
-            ans.append(list(ds))
+# def findCombinations(index, arr, ans, ds, target):
+#     if index == len(arr):
+#         if target == 0:
+#             ans.append(list(ds))
+#         return
+#     if arr[index] <= target:
+#         ds.append(arr[index])
+#         findCombinations(index, arr, ans, ds, target-arr[index])
+#         ds.pop()
+#     findCombinations(index+1, arr, ans, ds, target)
+
+
+# arr = [2, 3, 6, 7]
+# ans = []
+# findCombinations(0, arr, ans, [], 7)
+# print(ans)
+
+# #* Combinations Sum 2
+def findCombinations(index, arr, target, output, ds):
+    if target == 0:
+        output.append(list(ds))
         return
-    if arr[index] <= target:
-        ds.append(arr[index])
-        findCombinations(index, arr, ans, ds, target-arr[index])
+    for i in range(index, len(arr)):
+        if i > index and arr[i] == arr[i-1]:
+            continue
+        if arr[i] > target:
+            break
+        ds.append(arr[i])
+        findCombinations(i+1, arr, target-arr[i], output, ds)
         ds.pop()
-    findCombinations(index+1, arr, ans, ds, target)
 
 
-arr = [2, 3, 6, 7]
-ans = []
-findCombinations(0, arr, ans, [], 7)
-print(ans)
+arr = [10, 1, 2, 7, 6, 1, 5]
+arr.sort()
+target = 8
+output = []
+findCombinations(0, arr, target, output, [])
+print(output)
