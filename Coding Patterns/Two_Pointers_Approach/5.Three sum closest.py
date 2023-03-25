dfb99cb85875ee4bf2007eec020b645337ba1938
arslanjajja1 <arslanjajja13@gmail.com>
2023-03-25 12:21:40 +0500

    3 sum closest

---
+++ Coding Patterns/Two_Pointers_Approach/5.Three sum closest.py
@@ -0,0 +1,53 @@
+# 16. 3Sum Closest
+# Medium
+# 8.9K
+# 476
+# Companies
+# Given an integer array nums of length n and an integer target, find three integers in nums such that the sum is closest to target.
+
+# Return the sum of the three integers.
+
+# You may assume that each input would have exactly one solution.
+
+ 
+
+# Example 1:
+
+# Input: nums = [-1,2,1,-4], target = 1
+# Output: 2
+# Explanation: The sum that is closest to the target is 2. (-1 + 2 + 1 = 2).
+# Example 2:
+
+# Input: nums = [0,0,0], target = 1
+# Output: 0
+# Explanation: The sum that is closest to the target is 0. (0 + 0 + 0 = 0).
+ 
+
+# Constraints:
+
+# 3 <= nums.length <= 500
+# -1000 <= nums[i] <= 1000
+# -104 <= target <= 104
+
+def threeSumClosest(self, nums: List[int], target: int) -> int:
+        result = nums[0]+nums[1]+nums[len(nums)-1]
+        nums.sort()
+        for i in range(len(nums)-2):
+            left = i+1
+            right = len(nums)-1
+            while left < right:
+                current_sum = nums[i]+ nums[left]+nums[right]
+                if  (abs(current_sum-target)<abs(result-target)):
+                    result  = current_sum
+                if current_sum > target:
+                    right -= 1
+                else:
+                    left += 1
+           
+        return result
+
+# Time complexity #
+# Sorting the array will take O(N∗logN). Overall searchTriplet() will take O(N∗logN+N2), which is asymptotically equivalent to O(N2).
+
+# Space complexity #
+# The space complexity of the above algorithm will be O(N) which is required for sorting.
