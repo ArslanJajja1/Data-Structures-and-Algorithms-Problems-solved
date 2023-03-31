f5ff3a3a1c863d97fbadaedf366163b346e2d2c1
arslanjajja1 <arslanjajja13@gmail.com>
2023-03-31 15:46:38 +0500

    min stack with O(1) get min element in stack

--- Stacks/10.Min stack.py
+++ Stacks/10.Min stack.py
@@ -71,3 +71,27 @@ class MinStack:
 # obj.pop()
 # param_3 = obj.top()
 # param_4 = obj.getMin()
+
+#! with O(1) get minimum element
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.min_element = float('inf')
+
+    def push(self, val: int) -> None:
+        if val <= self.min_element:
+            self.stack.append(self.min_element)
+            self.min_element = val
+        self.stack.append(val)
+
+    def pop(self) -> None:
+        item = self.stack.pop()
+        if item == self.min_element:
+            self.min_element = self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.min_element
