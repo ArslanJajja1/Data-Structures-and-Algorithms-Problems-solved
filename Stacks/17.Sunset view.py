65efe00436785eb2951cb45a9c1cde54567c8422
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-01 16:19:58 +0500

    number of buildings facing sunset using stack

--- Stacks/17.Sunset view.py
+++ Stacks/17.Sunset view.py
@@ -14,6 +14,8 @@
 # Input : arr[] = {2, 3, 4, 5}
 # Output : 4
 
+# Without stack
+
 def sunset_view(buildings):
     count = 1
     curr_max = buildings[0]
@@ -24,3 +26,30 @@ def sunset_view(buildings):
     return count
 
 print(sunset_view([7, 4, 8, 2, 9]))
+
+# With stack
+
+def count_buildings_facing_sunset(buildings,direction):
+    stack = []
+    step = 1 if direction == "EAST" else -1
+    start = 0 if direction == "EAST" else len(buildings)-1
+    index = start
+    while index >= 0  and index < len(buildings):
+        while len(stack)>0 and buildings[stack[-1]] <= buildings[index]:
+            stack.pop()
+        stack.append(index)
+        index += step
+    return stack if direction == "EAST" else stack[::-1]
+print(count_buildings_facing_sunset([7, 4, 8, 2, 9],"EAST"))
+
+# With stack
+
+def sunet_view_2(buildings):
+    stack = []
+    stack.append(buildings[0])
+    for i in range(1,len(buildings)):
+        if buildings[i] > stack[-1]:
+            stack.append(buildings[i])
+    return len(stack)
+
+print(sunet_view_2([7, 4, 8, 2, 9]))
