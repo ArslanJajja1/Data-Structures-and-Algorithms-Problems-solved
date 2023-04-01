84a646b595e2164852699e70325fade53aa96705
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-01 14:03:37 +0500

    without stack implementation of car fleet

--- Stacks/15.Car fleets.py
+++ Stacks/15.Car fleets.py
@@ -49,6 +49,7 @@
 # All the values of position are unique.
 # 0 < speed[i] <= 106
 
+# With Stack
 class Solution:
     def carFleet(self, target: int, position: List[int], speed: List[int]) -> int:
         pair = [[p,s] for p,s in zip(position,speed)]
@@ -58,3 +59,18 @@ class Solution:
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
+
+    
+# Without Stack
+    def carFleet2(self, target: int, position: List[int], speed: List[int]) -> int:
+            pairs = [(position[i], speed[i]) for i in range(len(position))]
+            fleets = curtime = 0  # a car's position is always < than target at the start, so it's fine to start curtime at 0 (no fleet will be at target at time 0)
+            
+            for dist, speed in sorted(pairs, reverse=True):
+                destination_time = (target - dist)/speed
+                if curtime < destination_time:
+                    fleets += 1
+                    curtime = destination_time
+
+            return fleets
+
