9fbda85aa04f699deaf393faff50cbe01eccaab1
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-03 19:27:53 +0500

    # 33. Search in Rotated Sorted Array

---
+++ Binary Search/7.Search in rotated sorted array.py
@@ -0,0 +1,90 @@
+# 33. Search in Rotated Sorted Array
+# Medium
+# 20.7K
+# 1.2K
+# Companies
+# There is an integer array nums sorted in ascending order (with distinct values).
+
+# Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+# Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+# Example 1:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 0
+# Output: 4
+# Example 2:
+
+# Input: nums = [4,5,6,7,0,1,2], target = 3
+# Output: -1
+# Example 3:
+
+# Input: nums = [1], target = 0
+# Output: -1
+ 
+
+# Constraints:
+
+# 1 <= nums.length <= 5000
+# -104 <= nums[i] <= 104
+# All values of nums are unique.
+# nums is an ascending array that is possibly rotated.
+# -104 <= target <= 104
+
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        if n==1:
+            if nums[0] == target:
+                return 0
+            else:
+                return -1
+        min_element_index = self.find_minimum_element(nums)
+        left = self.binary_search(nums,target,0,min_element_index-1)
+        right = self.binary_search(nums,target,min_element_index,n-1)
+        if left == -1 and right == -1:
+            return -1
+        if left == -1:
+            return right
+        if right == -1:
+            return left
+
+    # Find the minimum element
+    def find_minimum_element(self,nums):
+        n = len(nums)
+        start = 0
+        end = n-1
+        while start<=end:
+            mid = start + (end-start)//2
+            prev = (mid-1+n)%n
+            nxt = (mid+1+n)%n
+            if nums[mid] < nums[prev] and nums[mid] < nums[nxt]:
+                return mid
+            elif nums[mid] <= nums[end]:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
+    # Binary search
+    def binary_search(self,nums,target,start,end):
+        n = len(nums)
+        while start<=end:
+            mid = start+(end-start)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        return -1
+
+# SC : O(1)
+# TC : O(logn)
+
+
+
+    
