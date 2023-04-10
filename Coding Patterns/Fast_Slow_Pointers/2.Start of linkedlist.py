908595be1e2e01def2ded9e6fc2fa67de9d15938
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-10 11:04:15 +0500

    start of linkedlist using fast slow pointers

--- Coding Patterns/Fast_Slow_Pointers/2.Start of linkedlist.py
+++ Coding Patterns/Fast_Slow_Pointers/2.Start of linkedlist.py
@@ -3,26 +3,54 @@ class ListNode:
     def __init__(self,val=0,next=None) -> None:
         self.val = val
         self.next = next
-def find_start_of_ll(head):
+# def find_start_of_ll(head):
+#         if not head or not head.next:
+#            return None
+#         hashmap = {}
+#         curr = head
+#         while curr != None:
+#             if curr not in hashmap:
+#                 hashmap[curr] = True
+#             else:
+#                 return curr.val
+#             curr = curr.next
+#         return None
+
+# head = ListNode(1)
+# head.next = ListNode(3)
+# head.next.next = ListNode(5)
+# head.next.next.next = ListNode(7)
+# print(find_start_of_ll(head))
+# head.next.next.next.next = head.next.next
+# print(find_start_of_ll(head))
+
+# TC : O(N)
+# SC : O(N)
+
+def start_of_ll(head):
         if not head or not head.next:
-           return None
-        hashmap = {}
-        curr = head
-        while curr != None:
-            if curr not in hashmap:
-                hashmap[curr] = True
-            else:
-                return curr.val
-            curr = curr.next
+                return None
+        slow = head
+        fast = head
+        entry = head
+
+        while fast and fast.next:
+           slow = slow.next
+           fast = fast.next.next
+           if slow == fast:
+                while slow != entry:
+                   slow = slow.next
+                   entry = entry.next
+                return slow
         return None
 
 head = ListNode(1)
 head.next = ListNode(3)
 head.next.next = ListNode(5)
 head.next.next.next = ListNode(7)
-print(find_start_of_ll(head))
+print(start_of_ll(head))
 head.next.next.next.next = head.next.next
-print(find_start_of_ll(head))
+print(start_of_ll(head))
 
 # TC : O(N)
-# SC : O(N)
+# SC : O(1)
