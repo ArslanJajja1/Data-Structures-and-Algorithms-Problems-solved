b05473051bf4e7851bab2bdd607726496788481f
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-12 20:54:57 +0500

    cyclic sort pattern

---
+++ Coding Patterns/Cyclic_Sort/0.Cyclic sort.py
@@ -0,0 +1,19 @@
+# Cyclic sort
+
+def cyclic_sort(nums):
+    i = 0
+    n = len(nums)
+    while i < n:
+        j = nums[i]-1
+        if nums[i] != nums[j]:
+            nums[i],nums[j] = nums[j],nums[i]
+        else:
+            i += 1
+    return nums
+
+print(cyclic_sort([3, 1, 5, 4, 2]))
+print(cyclic_sort([2, 6, 4, 3, 1, 5]))
+print(cyclic_sort([1, 5, 6, 4, 3, 2]))
+
+# TC : O(N)
+# SC : O(1)
