1b4fb9eb16cc31d7a481ed082d38c687ab3cc95d
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-12 20:55:09 +0500

    find missing number

---
+++ Coding Patterns/Cyclic_Sort/1.Missing Number.py
@@ -0,0 +1,17 @@
+# Find Missing number
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        i = 0
+        n = len(nums)
+        while i < n:
+            j = nums[i]
+            if nums[i] < n and nums[i] != nums[j]:
+                nums[i],nums[j] = nums[j],nums[i]
+            else:
+                i += 1
+        for i in range(n):
+            if nums[i] != i:
+                return i
+        return n
+# TC : O(N)
+# SC : O(1)
