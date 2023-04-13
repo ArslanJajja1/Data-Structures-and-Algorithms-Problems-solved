ca15c6c38b550fa433816379662e77a8ef51ed9d
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-13 16:58:07 +0500

    Reverse linkedlist

---
+++ Coding Patterns/Inplace_Reversals/0.Reverse linkedlist.py
@@ -0,0 +1,25 @@
+# 206. Reverse Linked List
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def reverseList(self, head: Optional[ListNode],prev=None) -> Optional[ListNode]:
+        # prev=None
+        # if not head:
+        #    return prev
+        # while head:
+        #     nextNode=head.next
+        #     head.next=prev
+        #     prev=head
+        #     head=nextNode
+        # return prev
+# TC : O(N)
+# SC : O(1)
+
+        if not head:
+            return prev
+        nextNode=head.next
+        head.next=prev
+        return self.reverseList(nextNode,head)
