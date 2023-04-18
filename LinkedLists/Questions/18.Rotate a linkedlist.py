f18a446daeef6004fad471cda85974b66e160d78
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-18 16:55:07 +0500

    rotate linkedlist

--- LinkedLists/Questions/18.Rotate a linkedlist.py
+++ LinkedLists/Questions/18.Rotate a linkedlist.py
@@ -8,16 +8,21 @@ class Solution:
         if not head or not head.next or not k:
             return head
         length = 1
-        temp = head
-        while temp.next:
-            length = length+1
-            temp = temp.next
-        temp.next = head
-        k = k % length
-        k = length-k
-        while temp and k > 0:
-            temp = temp.next
-            k = k-1
-        head = temp.next
-        temp.next = None
+        curr = head
+        while curr and curr.next:
+            length += 1
+            curr = curr.next
+        curr.next = head
+        k = k%length
+        k = length - k
+        while curr and k>0:
+            k -= 1
+            curr = curr.next
+
+        head = curr.next
+        curr.next = None
         return head
+            
+
+# TC : O(N)
+# SC : O(1)
