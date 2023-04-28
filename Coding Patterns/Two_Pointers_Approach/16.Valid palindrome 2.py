5b828d4850c0ae60d4b0648ca87006a42f3190ae
arslanjajja1 <arslanjajja13@gmail.com>
2023-04-28 17:18:54 +0500

    # 680. Valid Palindrome II

---
+++ Coding Patterns/Two_Pointers_Approach/16.Valid palindrome 2.py
@@ -0,0 +1,43 @@
+# 680. Valid Palindrome II
+# Easy
+# 7.1K
+# 368
+# Companies
+# Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+
+ 
+
+# Example 1:
+
+# Input: s = "aba"
+# Output: true
+# Example 2:
+
+# Input: s = "abca"
+# Output: true
+# Explanation: You could delete the character 'c'.
+# Example 3:
+
+# Input: s = "abc"
+# Output: false
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 105
+# s consists of lowercase English letters.
+
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s)-1
+        while left < right:
+            if s[left] != s[right]:
+                skipL,skipR = s[left+1:right+1],s[left:right]
+                return skipL == skipL[::-1] or skipR == skipR[::-1]
+            left += 1
+            right -= 1
+        return True
+
+# TC : O(N)
+# SC : O(1)
