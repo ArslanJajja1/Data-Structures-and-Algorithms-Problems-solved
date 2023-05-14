c0db04aba7ff5d7cfc750760f1f30eb514f2cd6a
arslanjajja1 <arslanjajja13@gmail.com>
2023-05-14 18:13:29 +0500

    # 105. Construct Binary Tree from Preorder and Inorder Traversal

---
+++ Trees/23.Construct binary tree.py
@@ -0,0 +1,69 @@
+# 105. Construct Binary Tree from Preorder and Inorder Traversal
+# Medium
+# 12.7K
+# 374
+# Companies
+# Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+
+ 
+
+# Example 1:
+
+
+# Input: preorder = [3,9,20,15,7], inorder = [9,3,15,20,7]
+# Output: [3,9,20,null,null,15,7]
+# Example 2:
+
+# Input: preorder = [-1], inorder = [-1]
+# Output: [-1]
+ 
+
+# Constraints:
+
+# 1 <= preorder.length <= 3000
+# inorder.length == preorder.length
+# -3000 <= preorder[i], inorder[i] <= 3000
+# preorder and inorder consist of unique values.
+# Each value of inorder also appears in preorder.
+# preorder is guaranteed to be the preorder traversal of the tree.
+# inorder is guaranteed to be the inorder traversal of the tree.
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder or not inorder:
+            return None
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1:mid+1],inorder[:mid])
+        root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:])
+        return root
+    # TC : O(N^2)
+    # SC : O(N)
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        def helper(preorder,inorder,map):
+            if not preorder or not inorder:
+                return None
+            root = TreeNode(preorder[0])
+            mid = map[preorder[0]]
+            root.left = self.buildTree(preorder[1:mid+1],inorder[:mid])
+            root.right = self.buildTree(preorder[mid+1:],inorder[mid+1:])
+            return root
+        map = {val:key for key,val in enumerate(inorder)}
+        return helper(preorder,inorder,map)
+
+       # TC : O(N)
+       # SC : O(N) 
