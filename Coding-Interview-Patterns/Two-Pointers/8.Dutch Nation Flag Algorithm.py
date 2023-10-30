4e9869743ab14790ddba94f38283d1f02c25ba08
arslanjajja1 <arslanjajja13@gmail.com>
2023-10-30 11:02:26 +0500

    dutch national flag algorithm

---
+++ Coding-Interview-Patterns/Two-Pointers/8.Dutch Nation Flag Algorithm.py
@@ -0,0 +1,34 @@
+# Problem Statement #
+# Given an array containing 0s, 1s and 2s, sort the array in-place. You should treat numbers of the array as objects, hence, we can’t count 0s, 1s, and 2s to recreate the array.
+
+# The flag of the Netherlands consists of three colors: red, white and blue; and since our input array also consists of three different numbers that is why it is called Dutch National Flag problem.
+
+# Example 1:
+
+# Input: [1, 0, 2, 1, 0]
+# Output: [0 0 1 1 2]
+# Example 2:
+
+# Input: [2, 2, 0, 1, 2, 0]
+# Output: [0 0 1 2 2 2 ]
+
+def dutch_national_flag_algorithm(arr):
+    i = 0
+    left = 0
+    right = len(arr)-1
+    while i <= right:
+        if arr[i] == 0:
+            arr[i],arr[left] = arr[left],arr[i]
+            left += 1
+            i += 1
+        elif arr[i] == 2:
+            arr[i],arr[right] = arr[right],arr[i]
+            right -= 1
+        else:
+            i += 1
+    return arr
+
+print(dutch_national_flag_algorithm([1, 0, 2, 1, 0]))
+print(dutch_national_flag_algorithm([2, 2, 0, 1, 2, 0]))
+
+
